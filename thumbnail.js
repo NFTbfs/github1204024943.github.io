430c9b20c1cfea1441ad07c71ff7fd6549492783
github1204024943 <80587936+github1204024943@users.noreply.github.com>
2021-07-02 11:22:36 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  let targetImage = document.querySelector("#smart-image");
-  targetImage.addEventListener("click", function() {
-    alert("点击了图片");
-    
+  if (targetImage.classList.contains("small")) {
+    targetImage.addEventListener("click", function() {
+    targetImage.classList.remove("small");  
 });
-  targetImage.classList.remove("small");
+} else {
+    targetImage.addEventListener("click", function() {
+    targetImage.classList.add("small"); 
+});
+}
 });
