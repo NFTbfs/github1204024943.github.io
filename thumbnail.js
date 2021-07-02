e367b52b07df86cc4495c9d778c666f4de404cd2
github1204024943 <80587936+github1204024943@users.noreply.github.com>
2021-07-02 11:05:16 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
   targetImage.addEventListener("click", function() {
     alert("点击了图片");
 });
-  let targetImage = document.querySelector("#smart-image");
+  
 });
